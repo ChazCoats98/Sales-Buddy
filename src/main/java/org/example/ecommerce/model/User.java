592ff9937b97c112a8d42a6b3420b153c9bad96a
Chaz Coats <chazcoats2@hotmail.com>
2024-11-13 13:27:15 -0500

    Adding bean validation constraints to user fields

--- src/main/java/org/example/ecommerce/model/User.java
+++ src/main/java/org/example/ecommerce/model/User.java
@@ -1,6 +1,8 @@
 package org.example.ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
 
 @Entity
 public class User {
@@ -10,7 +12,14 @@ public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String username;
+    @NotNull
+    @Email
     private String email;
+    @NotNull
+    @Pattern(regexp = "/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/\n",
+    message = "Invalid password. Password must contain atleast 1 of the following: " +
+            "uppercase letter, lowercase letter, number, special character")
     private String password;
 }
