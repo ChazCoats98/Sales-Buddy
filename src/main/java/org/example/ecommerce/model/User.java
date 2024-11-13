4d3c050fc80f0642e4db3e712e89e337f004455b
Chaz Coats <chazcoats2@hotmail.com>
2024-11-13 08:46:51 -0500

    Writing basic User class outline and initial persistent fields

--- src/main/java/org/example/ecommerce/model/User.java
+++ src/main/java/org/example/ecommerce/model/User.java
@@ -1,4 +1,16 @@
 package org.example.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class User {
+    /**
+     * User class.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
 }
