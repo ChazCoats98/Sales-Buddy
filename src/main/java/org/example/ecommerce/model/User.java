a799f9c482303b73cb45c022d671e4199de763ee
Chaz Coats <chazcoats2@hotmail.com>
2024-11-14 07:56:40 -0500

    Adding getter and setter methods to User class

--- src/main/java/org/example/ecommerce/model/User.java
+++ src/main/java/org/example/ecommerce/model/User.java
@@ -2,6 +2,8 @@ package org.example.ecommerce.model;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.*;
+import java.util.Set;
+import java.util.HashSet;
 
 
 @Entity
@@ -22,4 +24,41 @@ public class User {
     message = "Invalid password. Password must contain atleast 1 of the following: " +
             "uppercase letter, lowercase letter, number, special character")
     private String password;
+    @OneToMany
+    private Set<Order> orders = new HashSet<>();
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setOrders(Order order) {
+        this.orders.add(order);
+    }
 }
