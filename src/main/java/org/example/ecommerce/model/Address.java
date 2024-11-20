1050385305f5209bb4cd39e577e00acd34c0bdf1
Chaz Coats <chazcoats2@hotmail.com>
2024-11-20 14:03:27 -0500

    Adding getter and setter methods to Address class

--- src/main/java/org/example/ecommerce/model/Address.java
+++ src/main/java/org/example/ecommerce/model/Address.java
@@ -1,6 +1,7 @@
 package org.example.ecommerce.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 @Entity
 public class Address {
@@ -11,9 +12,54 @@ public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
     private String country;
+    @NotNull
     private String state;
+    @NotNull
     private String city;
+    @NotNull
     private String street;
+    @NotNull
     private String zipCode;
+
+    private String getCountry() {
+        return country;
+    }
+
+    private void setCountry(String country) {
+        this.country = country;
+    }
+
+    private String getState() {
+        return state;
+    }
+
+    private void setState(String state) {
+        this.state = state;
+    }
+
+    private String getCity() {
+        return city;
+    }
+
+    private void setCity(String city) {
+        this.city = city;
+    }
+
+    private String getStreet() {
+        return street;
+    }
+
+    private void setStreet(String street) {
+        this.street = street;
+    }
+
+    private String getZipCode() {
+        return zipCode;
+    }
+
+    private void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
